1ef7cd8e0073391673cc928218be4aa65092e420
HarshadSahu <HarshadS@emids.com>
2024-02-18 13:27:49 +0530

    code change

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This website has CI/CD on every commit!
-        </p>
-        <a
-          className="App-link"
-          href="https://thoughtstile.medium.com/setup-a-create-react-app-ci-cd-pipeline-on-every-github-commit-via-google-cloud-build-and-google-7ff6cb96cb1d"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github Commit -> Google Cloud Build -> Google Cloud Run
-        </a>
+        <p>This website has CI/CD on every commit!</p>
+        <h2>This is me, Harshad!</h2>
       </header>
     </div>
   );
